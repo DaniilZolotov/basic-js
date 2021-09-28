b88395856c5fbf21d603439f801883f3e28fcb2a
Daniil <zolotovdaniil2@gmail.com>
2021-09-28 13:06:05 +0300

    feat: st-sum-digits.js done

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
+
+ export default function createDreamTeam(/* members */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
+/*export default function createDreamTeam(members) {
+	let reg = /[A-Z]/g;
+	let arr = [];
+	for (let i=0; i<members.length; i++){
+	if(typeof members[i] === "string" && members[i].length>0){
+			arr.push(members[i].match(reg))
+	}
+}
+	let str = arr.sort().join(' ');
+	if(arr.length==0||typeof members !='object'){
+		return false;
+	}
+	else{
+		return str;
+	}
+}*/
